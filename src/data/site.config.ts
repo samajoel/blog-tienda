1ce20c22df23c9d65462ac63bb8af889cc9b9708
joel-samaniego <joel.samaniego@university.mentormate.com>
2023-10-25 16:11:00 -0300

    Changes

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
+	author: 'makeyeye', // Site author
+	title: 'looptech', // Site title.
+	description: 'Avanzamos contigo.', // Description to display in the meta tags
+	lang: 'es-ES',
+	ogLocale: 'es_ES',
+	shareMessage: 'Compartir', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
